a6844955dd0e0137b2a7d5f0d17fb412042aa29a
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-11-14 10:59:27 +0700

    das

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,17 +1,14 @@
 local url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/Main.lua"
 if type(_G.KaitunConfig) == "table" or _G.KaitunMode then
-  url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/Kaitun.lua"
-	return;
+	url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/Kaitun.lua"
 end
 
 if type(_G.V4Config) == "table" then
 	url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/V4.lua"
-	return;
 end
 
 if type(_G.BountyConfig) == "table" or _G.BountyMode then
 	url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/Bounty.lua"
-	return;
 end
 
 loadstring(game:HttpGet(url))()
