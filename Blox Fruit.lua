b05a226d20d540d06fb0a4721dbbd3ee68f9b7e8
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-11-21 19:24:40 +0700

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,3 +1,10 @@
+local a 
+a = hookmetamethod(workspace, "__index", function(self,key)
+    if key == "DataCost" then 
+        return math.random(100,900)
+    end
+    return a(self,key)
+end)
 local url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/Main.lua"
 if type(_G.KaitunConfig) == "table" or _G.KaitunMode then
 	url = "https://raw.githubusercontent.com/NightsTimeZ/RoyxBF/main/Kaitun.lua"
