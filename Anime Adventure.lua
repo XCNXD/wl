1fbc98e16c734ae74d74dd6c5bf6a7c83ba13bd8
KangKung <87695599+KangKung02@users.noreply.github.com>
2025-03-12 18:02:19 +0700

    Update Anime Adventure.lua

--- Anime Adventure.lua
+++ Anime Adventure.lua
@@ -1,4 +1,4 @@
-if _G.VersionControl == "V2" then
+if _G.VersionControl == "V2" or _G.RoyxSpecialMode == "Hell Factory" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/AAOld.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/AAnormal.lua"))()
